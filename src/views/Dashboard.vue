6d6a74aafce19be2d7eb5d17fed6ce03ecd020b5
Fish-IRL <huvuhuvuhuvu@gmail.com>
2019-11-24 11:32:19 +0700

    item group for cards

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,33 +3,28 @@
         <v-text-field label="Where would you like to eat?" prepend-inner-icon="mdi-magnify" solo outlined flat dense></v-text-field>
         <p class="font-weight-bold">Nearby Restaurants</p>
         <v-divider></v-divider>
-        <v-container>
-            <v-row>
-                <v-col v-for="card in cards" :key="card.title" :cols="card.flex" class="mx-0">
-                    <v-card max-height="200px" min-height="200px">
-                        <v-img :src="card.src" height="100px" class="mx-0"></v-img>
-                        <v-card-text class="text--primary">
-                            <div>{{ card.title }}</div>
-                        </v-card-text>
-                    </v-card>
-                </v-col>
-            </v-row>
-        </v-container>
+        <v-slide-group show-arrows class="my-2">
+            <v-slide-item v-for="card in cards" :key="card.title" class="my-2">
+                <v-card class="mx-2" width="175px">
+                    <v-img :src="card.src" height="100px"></v-img>
+                    <v-card-text class="text--primary">
+                        <div>{{ card.title }}</div>
+                    </v-card-text>
+                </v-card>
+            </v-slide-item>
+        </v-slide-group>
         <p class="font-weight-bold">Choose the perfect environment</p>
         <v-divider></v-divider>
-        <v-container>
-            <v-row>
-                <v-col v-for="card in cards2" :key="card.title" :cols="card.flex" class="mx-0">
-                    <v-card>
-                        <v-img :src="card.src" height="100px" class="mx-0"></v-img>
-                        <v-card-text class="text--primary">
-                            <div>{{ card.title }}</div>
-                        </v-card-text>
-                    </v-card>
-                </v-col>
-            </v-row>
-        </v-container>
-
+        <v-slide-group show-arrows class="my-2">
+            <v-slide-item v-for="card in cards2" :key="card.title" class="my-2">
+                <v-card class="mx-2" width="175px">
+                    <v-img :src="card.src" height="100px"></v-img>
+                    <v-card-text class="text--primary">
+                        <div>{{ card.title }}</div>
+                    </v-card-text>
+                </v-card>
+            </v-slide-item>
+        </v-slide-group>
     </v-container>
 </template>
 
@@ -37,12 +32,24 @@
 export default {
   data: () => ({
     cards: [
-      { title: 'Restaurant names', src: 'https://picsum.photos/id/1060/300', flex: 6 },
-      { title: 'Restaurant names', src: 'https://picsum.photos/id/1060/300', flex: 6 }
+      { title: 'Restaurant names', src: 'https://picsum.photos/id/1060/300' },
+      { title: 'Restaurant names', src: 'https://picsum.photos/id/1060/300' },
+      { title: 'Restaurant names', src: 'https://picsum.photos/id/1060/300' },
+      { title: 'Restaurant names', src: 'https://picsum.photos/id/1060/300' },
+      { title: 'Restaurant names', src: 'https://picsum.photos/id/1060/300' },
+      { title: 'Restaurant names', src: 'https://picsum.photos/id/1060/300' },
+      { title: 'Restaurant names', src: 'https://picsum.photos/id/1060/300' },
+      { title: 'Restaurant names', src: 'https://picsum.photos/id/1060/300' }
     ],
     cards2: [
-      { title: 'Family Friendly', src: 'https://picsum.photos/id/1060/300', flex: 6 },
-      { title: 'Romantic Dinner', src: 'https://picsum.photos/id/1060/300', flex: 6 }
+      { title: 'Family Friendly', src: 'https://picsum.photos/id/1060/300' },
+      { title: 'Romantic Dinner', src: 'https://picsum.photos/id/1060/300' },
+      { title: 'Open Area', src: 'https://picsum.photos/id/1060/300' },
+      { title: 'Silent', src: 'https://picsum.photos/id/1060/300' },
+      { title: 'Text', src: 'https://picsum.photos/id/1060/300' },
+      { title: 'Text', src: 'https://picsum.photos/id/1060/300' },
+      { title: 'Text', src: 'https://picsum.photos/id/1060/300' },
+      { title: 'Text', src: 'https://picsum.photos/id/1060/300' }
     ]
   })
 }
