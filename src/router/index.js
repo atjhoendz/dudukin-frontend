6a604c4aa09e85f931d0925996c45dc50257311c
Fish-IRL <huvuhuvuhuvu@gmail.com>
2019-11-26 23:27:17 +0700

    RestaurantPage route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Dashboard from '../views/Dashboard.vue'
+import RestaurantPage from '../views/RestaurantPage.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/dashboard',
     name: 'dashboard',
     component: Dashboard
+  },
+  {
+    path: '/restaurant/1',
+    name: 'restaurant',
+    component: RestaurantPage
   },
   {
     path: '/about',
