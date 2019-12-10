a4c2740bd6f616dfa60fdc772e97fc992f264652
Fish-IRL <huvuhuvuhuvu@gmail.com>
2019-12-11 00:51:34 +0700

    add onclick event for profile icon

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
         <v-app-bar flat dense app class="deep-orange">
             <v-spacer></v-spacer>
             <v-avatar>
-                <v-icon large>mdi-account-circle</v-icon>
+                <v-icon large onClick="window.location.href='/profile'">mdi-account-circle</v-icon>
             </v-avatar>
         </v-app-bar>
     </div>
